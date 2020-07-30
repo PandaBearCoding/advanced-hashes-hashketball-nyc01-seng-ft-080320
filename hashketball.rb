c493d340fdf1c9c2dc9a6f85572013fdcdf0987b
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-30 20:28:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -187,8 +187,7 @@ end
 def player_stats(name)
 game_hash.each do |key, value|
 value[:players].each do |player_name|
-  if name == player_name[:player_name]
-  hash_of_players_stats << :player_name.delete(:player_name)
+  if name == player_name[:player_name] return player_name
   end
   end
   end
