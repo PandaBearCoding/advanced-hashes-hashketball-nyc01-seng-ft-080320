cddebbecda1eeff6481d3da101109e2e7cdb1303
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-30 20:43:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -197,6 +197,12 @@ value[:players].each do |player_name|
 end
 
 
+So at this part , if name == player_name[:player_name] return player_name
+
+Remove the delete stuff and the empty hash in the beginning
+
+
+
 def big_shoe_rebounds
   biggest_shoe = 0
   num_rebounds = 0
