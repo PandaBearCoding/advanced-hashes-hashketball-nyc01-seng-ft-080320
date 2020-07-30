3a6f183369fbbb8829b52885e87c4bf9e3ffbe32
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-07-30 20:32:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -185,18 +185,18 @@ end
 
 
 def player_stats(name)
-#hash_of_players_stats = {}
+hash_of_players_stats = {}
 game_hash.each do |key, value|
 value[:players].each do |player_name|
-  #if name == player_name[:player_name]
-  #hash_of_players_stats << :player_name.delete(:player_name)
+  if name == player_name[:player_name]
+  hash_of_players_stats << :player_name.delete(:player_name)
   
   
-  if name == player_name[:player_name] return player_name
+  So at this part , if name == player_name[:player_name] return player_name
 
 
   
-  #end
+  end
   end
   end
   hash_of_players_stats
